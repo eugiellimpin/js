8c05c02dc00cb036c3372020ed5a554009640bf2
Eugie Limpin <eugiellimpin@gmail.com>
2021-02-11 03:51:08 +0800

    Add answers for rest of chapter 3 exercises

--- eloquent.js
+++ eloquent.js
@@ -129,7 +129,110 @@ function min(x, y) {
 //   return Math.min(x, y);
 // }
 
-console.log('Min of 0 and 10 is', min(0, 10));
-console.log('Min of 0 and -10 is', min(0, -10));
+console.log("Min of 0 and 10 is", min(0, 10));
+console.log("Min of 0 and -10 is", min(0, -10));
+
+console.log("==================================================\n");
+
+/*
+Recursion
+
+We’ve seen that % (the remainder operator) can be used to test whether a
+number is even or odd by using % 2 to see whether it’s divisible by two.
+Here’s another way to define whether a positive whole number is even or odd:
+
+- Zero is even.
+- One is odd.
+- For any other number N, its evenness is the same as N - 2.
+
+Define a recursive function isEven corresponding to this description. The
+function should accept a single parameter (a positive, whole number) and
+return a Boolean.
+
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way
+to fix this?
+
+// Your code here.
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+*/
+
+console.log("Recursion");
+console.log("==================================================");
+
+function isEven(n) {
+  if (n < 0) return false;
+  if (n === 0) return true;
+  if (n === 1) return false;
+  return isEven(n - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
+console.log("==================================================\n");
+
+/*
+Bean counting
+
+You can get the Nth character, or letter, from a string by writing
+"string"[N]. The returned value will be a string containing only one
+character (for example, "b"). The first character has position 0, which
+causes the last one to be found at position string.length - 1. In other
+words, a two-character string has length 2, and its characters have positions
+0 and 1.
+
+Write a function countBs that takes a string as its only argument and returns
+a number that indicates how many uppercase “B” characters there are in the
+string.
+
+Next, write a function called countChar that behaves like countBs, except it
+takes a second argument that indicates the character that is to be counted
+(rather than counting only uppercase “B” characters). Rewrite countBs to make
+use of this new function.
+
+// Your code here.
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+*/
+
+console.log("Bean counting");
+console.log("==================================================");
+
+// initial
+// function countBs(s) {
+//   // return s
+//   //   .split("")
+//   //   .reduce((count, c) => (c === "B" ? count + 1 : count), 0);
+
+//   let count = 0;
+//   for (const c of s) {
+//     if (c === 'B') count++;
+//   }
+//   return count;
+// }
+
+function countChar(_string, _char) {
+  return _string
+    .split("")
+    .reduce((count, c) => (c === _char ? count + 1 : count), 0);
+}
+
+// rewrite
+function countBs(s) {
+  return countChar(s, "B");
+}
+
+console.log(countBs("BBC"));
+console.log(countChar("kakkerlak", "k"));
 
 console.log("==================================================\n");
