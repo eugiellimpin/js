0c028c3cc7a29049e0b508d9fff4bab83723e075
Eugie Limpin <eugiellimpin@gmail.com>
2021-02-15 11:14:00 +0800

    Add Chapter 4 Exercises 2 & 3

--- eloquent.js
+++ eloquent.js
@@ -321,3 +321,217 @@ console.log(sum(range(1, 10)));
 // → 55
 
 console.log("==================================================\n");
+
+/*
+Reversing an array
+
+Arrays have a reverse method that changes the array by inverting the order in
+which its elements appear. For this exercise, write two functions,
+reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array
+as argument and produces a new array that has the same elements in the
+inverse order. The second, reverseArrayInPlace, does what the reverse method
+does: it modifies the array given as argument by reversing its elements.
+Neither may use the standard reverse method.
+
+Thinking back to the notes about side effects and pure functions in the
+previous chapter, which variant do you expect to be useful in more
+situations? Which one runs faster?
+
+// Your code here.
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+*/
+
+console.log("Reversing an array");
+console.log("==================================================");
+
+// Using reduceRight
+// function reverseArray(a) {
+//   return a.reduceRight((result, e) => {
+//     result.push(e);
+//     return result
+//   }, []);
+// }
+
+function reverseArray(a) {
+  const reversed = [];
+  for (let i = a.length - 1; i >= 0; i--) {
+    reversed.push(a[i]);
+  }
+  return reversed;
+}
+
+// function reverseArrayInPlace(orig) {
+//   const copy = orig.slice();
+//   for (let i = 0; i < orig.length; i++) {
+//     orig[i] = copy[orig.length - 1 - i];
+//   }
+// }
+
+// Without using slice or reverseArray
+function reverseArrayInPlace(orig) {
+  // if length is 5 then 2 (third element) since floor of 2.5 is 2
+  // if length is 4 then 2 (third element)
+  const oneAfterMidIndex = Math.floor(orig.length / 2);
+
+  // Swap mirror elements without touching middle element (one without a mirror)
+  // if there is one
+  //
+  // Given [1, 2, 3, 4, 5] do,
+  // 1. swap 1 & 5
+  // 2. swap 2 & 4
+  for (let i = 0; i < oneAfterMidIndex; i++) {
+    const mirrorElement = orig[orig.length - 1 - i];
+    orig[orig.length - 1 - i] = orig[i];
+    orig[i] = mirrorElement;
+  }
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+
+console.log("==================================================");
+
+/*
+A list
+
+Objects, as generic blobs of values, can be used to build all sorts of data
+structures. A common data structure is the list (not to be confused with
+array). A list is a nested set of objects, with the first object holding a
+reference to the second, the second to the third, and so on.
+
+let list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+
+A nice thing about lists is that they can share parts of their structure. For
+example, if I create two new values {value: 0, rest: list} and {value: -1,
+rest: list} (with list referring to the binding defined earlier), they are
+both independent lists, but they share the structure that makes up their last
+three elements. The original list is also still a valid three-element list.
+
+Write a function arrayToList that builds up a list structure like the one
+shown when given [1, 2, 3] as argument. Also write a listToArray function
+that produces an array from a list. Then add a helper function prepend, which
+takes an element and a list and creates a new list that adds the element to
+the front of the input list, and nth, which takes a list and a number and
+returns the element at the given position in the list (with zero referring to
+the first element) or undefined when there is no such element.
+
+If you haven’t already, also write a recursive version of nth.
+
+// Your code here.
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+*/
+
+console.log("A list");
+console.log("==================================================");
+
+// Recursive versions
+// function arrayToList(items) {
+//   const [value, ...rest] = items;
+
+//   if (value === undefined) {
+//     return null;
+//   }
+
+//   return { value, rest: arrayToList(rest) };
+// }
+
+// function listToArray(list) {
+//   if (!list) return [];
+//   const { value, rest } = list;
+//   return [value].concat(listToArray(rest));
+// }
+
+// function nth(list, index) {
+//   if (!list) return undefined;
+//   if (index === 0) return list.value;
+//   const { rest } = list;
+//   return nth(rest, index - 1);
+// }
+
+function prepend(value, list) {
+  return { value, rest: list };
+}
+
+function arrayToList(items) {
+  let currentItem = null;
+
+  for (let i = items.length - 1; i >= 0; i--) {
+    currentItem = { value: items[i], rest: currentItem };
+  }
+
+  return currentItem === null ? undefined : currentItem;
+}
+
+function listToArray(list) {
+  if (!list) return [];
+
+  const array = [];
+
+  let _list = list;
+  do {
+    const { value, rest } = _list;
+    array.push(value);
+    _list = rest;
+  } while(!!_list)
+
+  return array;
+}
+
+function nth(list, index) {
+  if (!list) return undefined;
+  if (index === 0) return list.value;
+
+  let i = 1,
+    _list = list.rest;
+
+  while (i < index) {
+    _list = _list.rest;
+    i++;
+  }
+
+  return _list.value;
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(arrayToList([]));
+// → undefined
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+console.log(nth(arrayToList([10, 20, 30]), 2));
+// → 30
+console.log(nth(arrayToList([]), 2));
+// → undefined
+
+console.log("==================================================");
