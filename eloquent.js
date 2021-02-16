e73e758dbb02b051df889e1b5009a6e53b5d8d4c
Eugie Limpin <eugiellimpin@gmail.com>
2021-02-16 17:51:50 +0800

    Answer Chapter 4 Exercise 4

--- eloquent.js
+++ eloquent.js
@@ -536,6 +536,71 @@ console.log(nth(arrayToList([]), 2));
 
 console.log("==================================================");
 
+/*
+Deep comparison
+
+The == operator compares objects by identity. But sometimes you’d prefer to
+compare the values of their actual properties.
+
+Write a function deepEqual that takes two values and returns true only if
+they are the same value or are objects with the same properties, where the
+values of the properties are equal when compared with a recursive call to
+deepEqual.
+
+To find out whether values should be compared directly (use the === operator
+for that) or have their properties compared, you can use the typeof operator.
+If it produces "object" for both values, you should do a deep comparison. But
+you have to take one silly exception into account: because of a historical
+accident, typeof null also produces "object".
+
+The Object.keys function will be useful when you need to go over the
+properties of objects to compare them.
+*/
+
+console.log("Deep comparison");
+console.log("==================================================");
+
+// Your code here.
+
+function deepEqual(obj1, obj2) {
+  // If either one of the two is not an object then use ===
+  if (typeof obj1 !== "object" || typeof obj2 !== "object") {
+    return obj1 === obj2;
+  }
+
+  // If either one of the two is a null then use ===
+  if (obj1 === null || obj2 === null) {
+    return obj1 === obj2;
+  }
+
+  // If both are objects but their number of properties don't match then they're
+  // not equal
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+
+  return Object.keys(obj1).every((prop) => deepEqual(obj1[prop], obj2[prop]));
+}
+
+console.log(deepEqual(null, undefined));
+// false
+console.log(deepEqual(1, {}));
+// false
+console.log(deepEqual(null, null));
+// true
+console.log(deepEqual({ a: "a" }, { a: "a", b: "b" }));
+// false
+console.log(deepEqual({ a: "a" }, { a: "c" }));
+let obj = { here: { is: "an" }, object: 2 };
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, { here: 1, object: 2 }));
+// → false
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
+// → true
+console.log(deepEqual([1, 2], [1, 2]));
+// → true
+
+console.log("==================================================");
+
 // Chapter 5
 
 /*
