0f6f078eff872c7ab9bfcb57f9f5ec9344291e85
Eugie Limpin <eugiellimpin@gmail.com>
2021-02-21 16:06:16 +0800

    Answer Chapter 6 Exercise 2

--- chapter6.js
+++ chapter6.js
@@ -48,3 +48,87 @@ console.log(new Vec(3, 4).length);
 
 console.log("==================================================\n");
 
+
+/*
+Groups
+
+The standard JavaScript environment provides another data structure
+called Set. Like an instance of Map, a set holds a collection of values.
+Unlike Map, it does not associate other values with those—it just tracks
+which values are part of the set. A value can be part of a set only
+once—adding it again doesn’t have any effect.
+
+Write a class called Group (since Set is already taken). Like Set, it has
+add, delete, and has methods. Its constructor creates an empty group, add
+adds a value to the group (but only if it isn’t already a member), delete
+removes its argument from the group (if it was a member), and has returns a
+Boolean value indicating whether its argument is a member of the group.
+
+Use the === operator, or something equivalent such as indexOf, to determine
+whether two values are the same.
+
+Give the class a static from method that takes an iterable object as argument
+and creates a group that contains all the values produced by iterating over
+it.
+*/
+
+console.log("Groups");
+console.log("==================================================");
+
+class Group {
+  constructor() {
+    this.members = [];
+  }
+
+  add(m) {
+    if (!this.has(m)) {
+      this.members.push(m);
+    }
+  }
+
+  delete(m) {
+    if (this.has(m)) {
+      const index = this.members.indexOf(m);
+      this.members.splice(index, 1)
+    }
+  }
+
+  has(m) {
+    return this.members.indexOf(m) !== -1;
+  }
+
+  static from(iterable) {
+    const group = new Group();
+
+    for(const i of iterable) {
+      group.add(i);
+    }
+
+    return group;
+  }
+}
+
+// let g1 = new Group();
+// g1.add(10);
+// g1.add(10);
+// console.log(g1);
+// console.log(g1.has(10));
+// console.log(g1.has(1));
+// g1.add(20);
+// console.log(g1.has(20));
+// console.log(g1);
+// g1.delete(20);
+// console.log(g1);
+// console.log(Group.from([1,2,2]))
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
+
+console.log("==================================================\n");
