61dac111be75b54e7047b975488215ceec5bdefe
Eugie Limpin <eugiellimpin@gmail.com>
2021-02-21 15:28:44 +0800

    Answer the rest of Chapter 5

--- eloquent.js
+++ eloquent.js
@@ -1,3 +1,5 @@
+require('./scripts.js')
+
 // Chapter 2
 
 /*
@@ -623,5 +625,152 @@ function flat(arrayOfArrays) {
   return arrayOfArrays.reduce((flattened, arr) => flattened.concat(arr));
 }
 console.log(flat(arrays));
+
+console.log("==================================================");
+
+/*
+Your own loop
+
+Write a higher-order function loop that provides something like a for loop
+statement. It takes a value, a test function, an update function, and a body
+function. Each iteration, it first runs the test function on the current loop
+value and stops if that returns false. Then it calls the body function,
+giving it the current value. Finally, it calls the update function to create
+a new value and starts from the beginning.
+
+When defining the function, you can use a regular loop to do the actual
+looping.
+
+// Your code here.
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
+*/
+
+console.log("Your own loop");
+console.log("==================================================");
+
+function loop(counter, condition, updateCounter, loopBody) {
+  let _counter = counter;
+
+  while(condition(_counter)) {
+    loopBody(_counter);
+    _counter = updateCounter(_counter);
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+loop(0, n => n < 10, n => n + 1, console.log);
+
+console.log("==================================================");
+
+/*
+Everything
+
+Analogous to the some method, arrays also have an every method. This one
+returns true when the given function returns true for every element in the
+array. In a way, some is a version of the || operator that acts on arrays,
+and every is like the && operator.
+
+Implement every as a function that takes an array and a predicate function as
+parameters. Write two versions, one using a loop and one using the some
+method.
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+*/
+
+console.log("Everything");
+console.log("==================================================");
+
+// Loop version
+// function every(array, predicate) {
+//   let result = true;
+//   for (const e of array) {
+//     result = result && predicate(e);
+//   }
+//   return result;
+// }
+
+// Using some
+function every(array, predicate) {
+  // If some do not satisfy the predicate then not every element satisfy the predicate
+  return !array.some(e => !predicate(e));
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
+console.log("==================================================");
+
+/*
+Dominant writing direction
+
+Write a function that computes the dominant writing direction in a string of
+text. Remember that each script object has a direction property that can be
+"ltr" (left to right), "rtl" (right to left), or "ttb" (top to bottom).
+
+The dominant direction is the direction of a majority of the characters that
+have a script associated with them. The characterScript and countBy functions
+defined earlier in the chapter are probably useful here.
+*/
+
+console.log("Dominant writing direction");
+console.log("==================================================");
+
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+function dominantDirection(text) {
+  const counts = countBy(text, char => {
+    const script = characterScript(char.codePointAt(0));
+    return script ? script.direction : 'unknown';
+  });
+
+  let dominant = { name: 'unknown', count: -Infinity };
+  for (const group of counts) {
+    if (group.count > dominant.count) {
+      dominant = group;
+    }
+  }
+  return dominant.name;
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
 
 console.log("==================================================");
